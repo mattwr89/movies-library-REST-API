107c1e35ff01b49bc8947b5a24abc45ebd7652f4
mattwr89 <maryniak.mat@gmail.com>
2020-04-16 12:20:26 +0200

    CRUD methods added

--- src/main/java/pl/mattwr89/moviesapp/Controllers/MovieApiController.java
+++ src/main/java/pl/mattwr89/moviesapp/Controllers/MovieApiController.java
@@ -1,9 +1,6 @@
 package pl.mattwr89.moviesapp.Controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.mattwr89.moviesapp.model.Movie;
 
 import java.time.LocalDate;
@@ -12,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/movies/")
+@RequestMapping("/api/movies")
 public class MovieApiController {
 
     private  List<Movie> movies;
@@ -21,8 +18,8 @@ public class MovieApiController {
     public MovieApiController() {
        movies = new ArrayList<>();
        movies.add(new Movie(1L,"John Wick", LocalDate.of(2014,1,5)));
-       movies.add(new Movie(1L,"Django", LocalDate.of(2012,5,17)));
-       movies.add(new Movie(1L,"Rambo", LocalDate.of(1982,11,3)));
+       movies.add(new Movie(2L,"Django", LocalDate.of(2012,5,17)));
+       movies.add(new Movie(3L,"Rambo", LocalDate.of(1982,11,3)));
     }
 
     @GetMapping("/all")
@@ -36,6 +33,24 @@ public class MovieApiController {
         Optional<Movie> first = movies.stream().
                 filter(element -> element.getId() == index).findFirst();
         return first.get();
+
+    }
+
+    @PostMapping
+    public boolean addMovie(@RequestBody Movie movie){
+        return movies.add(movie);
+
+    }
+
+    @PutMapping
+    public boolean updateMovie(@RequestBody Movie movie){
+        return movies.add(movie);
+
+    }
+
+    @DeleteMapping
+    public boolean deleteMovie(@RequestParam int index){
+        return movies.removeIf(element -> element.getId() == index);
 
     }
 }
